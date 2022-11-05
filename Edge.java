3b67c4d2f4958c8b7948856ff0d2080b4c8dd04b
DENAHAK <114286260+DENAHAK@users.noreply.github.com>
2022-11-05 18:22:46 +0300

    Add files via upload

---
+++ Edge.java
@@ -0,0 +1,10 @@
+public class Edge {
+
+    public static void main (String[]args ){
+        extracted();
+    }
+
+    private static void extracted() {
+        System.out.println("dena ");
+    }
+}
